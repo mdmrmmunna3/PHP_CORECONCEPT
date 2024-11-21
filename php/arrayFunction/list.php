36bb5adadf84b828883d60d7f7efb98c1cd0c16c
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2024-11-21 11:10:50 +0600

    apply list and explored

--- php/arrayFunction/list.php
+++ php/arrayFunction/list.php
@@ -1,6 +1,12 @@
 <?php 
-$myArray = ["Cow", "Dog", "Cat"];
+$myArray = array("Cow ", " Dog ", " Cat");
 
 list($a, $b, $c) = $myArray;
 echo "I have serval animal: ", $a, $c, $b;
+
+
+$str = "<br> Hello iam explored!";
+
+// echo json_encode(explode(" ",$str));
+print_r(explode(" ",$str));
 ?>
