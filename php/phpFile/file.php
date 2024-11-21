32d280e93072c2a05b4f75d08141ea574bbbfd4c
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2024-11-21 12:39:14 +0600

    store data in file.txt file

---
+++ php/phpFile/file.php
@@ -0,0 +1,5 @@
+<?php 
+$txt = "Hello How are you! Good Morning, What are you doing now?";
+
+file_put_contents("file.txt", $txt);
+?>
---
+++ test/concept.md
@@ -0,0 +1,3 @@
+### isset(), list(), file_put_contents(), nl2br, PHP_EOL; 
+### readfile(),fopen(), fread(), fwrite(), fclose()
+
