9b13d7277b09947c5f4f291ae6c8041657adb81c
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2024-11-30 10:59:24 +0600

    apply regex function and modifier

--- regEx/regExFunction/main.php
+++ regEx/regExFunction/main.php
@@ -22,6 +22,13 @@ $sadResult = preg_filter('/[0-9]+/', '$0', $input);
 
 print_r($sadResult);
 
+echo "<br>";
+
+$matchText = "Surprise";
+$patt = "/Surprie/i";
+
+echo preg_match($patt, $matchText);
+echo "<br>";
 
 // SEARCH FOR STRING FIRST INDEX...
 $input2 = [
@@ -37,11 +44,30 @@ $input2 = [
 
 $query = "/papi/i";
 
-// $check = preg_grep("/^b/i", $input2);
-// print_r($check);
+$check = preg_grep("/^b/i", $input2);
+print_r($check);
+echo "<br>";
 
 // check for match value; and retrun 0 and 1;
 foreach($input2 as $line2) {
     echo preg_match(  $query, $line2, $matches);
 }
+
+echo "<br>";
+// modify text capitalaze 
+$cap = "hello php";
+echo " capitalize just first: ";
+echo ucfirst($cap); // capitalize just first sentence word;
+echo "<br>";
+echo "capitalize all: ";
+echo ucwords($cap); // capitalize all sentence words;
+
+echo "<br>" ;
+
+$hi = 'banglaDesh';
+echo "To LowerCase: ";
+echo strtolower($hi);
+echo "<br>";
+echo "To UpperCase: ";
+echo strtoupper($hi);
 ?>
